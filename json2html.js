c4a4ca16edafe05fd067cb5c32716610c97f972a
Sanjana-Janagani <146768007+Sanjana-Janagani@users.noreply.github.com>
2024-11-07 20:02:21 +0530

    Added json2html file

---
+++ json2html.js
@@ -0,0 +1,22 @@
+// json2html.js
+export default function json2html(data) {
+    let table = '<table data-user="janaganisanjana613@gmail.com">';
+    table += '<thead>';
+    table += '<tr><th>Name</th><th>Age</th><th>Gender</th></tr>';
+    table += '</thead>';
+    table += '<tbody>';
+  
+    data.forEach(item => {
+      table += '<tr>';
+      table += `<td>${item.Name}</td>`;
+      table += `<td>${item.Age}</td>`;
+      table += `<td>${item.Gender || ''}</td>`; 
+      table += '</tr>';
+    });
+  
+    table += '</tbody>';
+    table += '</table>';
+  
+    return table;
+  }
+  
